98aac51970e57cf689bf94e6da7e57cb94a7c81a
RAVINGAR <ravingarinc@gmail.com>
2023-10-30 15:13:58 +0800

    Remove action holder

--- gui/src/main/java/com/ravingarinc/api/gui/component/action/RunnableAction.java
+++ gui/src/main/java/com/ravingarinc/api/gui/component/action/RunnableAction.java
@@ -1,19 +1,12 @@
 package com.ravingarinc.api.gui.component.action;
 
 import com.ravingarinc.api.gui.BaseGui;
-import com.ravingarinc.api.gui.api.ActionHolder;
 import org.bukkit.entity.Player;
 
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 
 public class RunnableAction extends Action {
     private final BiConsumer<BaseGui, Player> consumer;
-
-    @Deprecated
-    public RunnableAction(final Consumer<ActionHolder> consumer) {
-        this((gui, p) -> consumer.accept(new ActionHolder(gui, p)));
-    }
 
     public RunnableAction(final Runnable runnable) {
         this((g, p) -> runnable.run());
