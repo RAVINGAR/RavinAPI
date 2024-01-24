591b6051b054b45cfb9f750dd2a8dec39f61c124
RAVINGAR <ravingarinc@gmail.com>
2024-01-24 22:14:47 +0800

    Minor GUI update.

--- gui/src/main/java/com/ravingarinc/api/gui/component/action/Action.java
+++ gui/src/main/java/com/ravingarinc/api/gui/component/action/Action.java
@@ -3,6 +3,8 @@ package com.ravingarinc.api.gui.component.action;
 import com.ravingarinc.api.gui.BaseGui;
 import org.bukkit.entity.Player;
 
+import java.util.function.BiConsumer;
+
 public abstract class Action implements Comparable<Action> {
     protected final String pointer;
     private final Integer priority;
@@ -30,5 +32,18 @@ public abstract class Action implements Comparable<Action> {
     @Override
     public int compareTo(final Action other) {
         return priority.compareTo(other.getPriority());
+    }
+
+    public static Action createMiscAction(final BiConsumer<BaseGui, Player> action) {
+        return createMiscAction(2, action);
+    }
+
+    public static Action createMiscAction(final int priority, final BiConsumer<BaseGui, Player> action) {
+        return new Action("MISC_ACTION", priority) {
+            @Override
+            public void performAction(BaseGui gui, Player performer) {
+                action.accept(gui, performer);
+            }
+        };
     }
 }
