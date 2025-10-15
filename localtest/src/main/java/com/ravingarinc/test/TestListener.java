ee9f0369c5ca8ed3ae165523ca6eedb2017fcd55
RAVINGAR <81618382+RAVINGAR@users.noreply.github.com>
2025-10-15 20:36:02 +0800

    REMOVE external plugin just in case.

--- localtest/src/main/java/com/ravingarinc/test/TestListener.java
+++ localtest/src/main/java/com/ravingarinc/test/TestListener.java
@@ -1,21 +1,12 @@
 package com.ravingarinc.test;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.BlockPosition;
-import com.ravingarinc.api.I;
 import org.bukkit.event.Listener;
 
-import java.util.logging.Level;
-
 public class TestListener implements Listener {
-    private final ProtocolManager protocol = ProtocolLibrary.getProtocolManager();
+    //private final ProtocolManager protocol = ProtocolLibrary.getProtocolManager();
 
     public TestListener(TestPlugin plugin) {
+        /*
         protocol.addPacketListener(new PacketAdapter(plugin, ListenerPriority.NORMAL, PacketType.Play.Client.BLOCK_DIG) {
             @Override
             public void onPacketReceiving(PacketEvent event) {
@@ -23,6 +14,6 @@ public class TestListener implements Listener {
                 BlockPosition pos = event.getPacket().getBlockPositionModifier().read(0);
                 I.log(Level.INFO, "Got block at " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ());
             }
-        });
+        });*/
     }
 }
